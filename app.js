027f2a70f7da8aee20cf54e22d546f1ddd7a3cfb
SabitCanTurunc <turuncsc@gmail.com>
2023-12-15 07:12:41 +0300

    get-create methods

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ import { getNotes, getNote, createNote } from './database.js'
 const app = express()
 app.use(express.json())
 
+app.get("/notes", async (req, res) => {
+
+  const notes = await getNotes()
+  res.send(notes)
+ 
+})
+
+app.get("/notes/:id", async (req, res) => {
+
+  const id = req.params.id
+  const note = await getNote(id)
+  res.status(201).send(note)
+})
+
+app.post("/notes",async (req,res)=>{
+const {title, contents}= req.body
+const note = await createNote(title,contents)
+res.send(note)
+})
 
 
 app.use((err, req, res, next) => {
